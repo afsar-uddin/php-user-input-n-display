cd2b02cf40229a2bfc72b6f27ab3f88ac5680e82
afsar-uddin <afsarwebdev@gmail.com>
2023-08-07 22:47:03 +0600

    data insert to db and display to front side with image

--- display.php
+++ display.php
@@ -6,11 +6,30 @@
     if(isset($_POST['submit'])) {
         $name = $_POST['name'];
         $phone = $_POST['phone'];
-        $image = $_FILES['file']['name'];
+        $file = $_FILES['file'];
+        $file_name = $file['name'];
+        $file_tmp = $file['tmp_name'];
 
-        echo $name . '</br>';
-        echo $phone . '</br>';
-        echo $image;
+        $file_name_separate = explode('.', $file_name);
+        $file_name_extension = end($file_name_separate);
+
+        $expected_extensios = array('jpeg', 'jpg', 'png');
+        if(in_array($file_name_extension, $expected_extensios)) {
+            $upload_img = 'images/'.$file_name;
+
+            move_uploaded_file($file_tmp, $upload_img);
+            
+            $sql = "insert into `users` (name, phone, image) values ('$name', '$phone', '$upload_img')";
+            $resuts = mysqli_query($con, $sql);
+            if($resuts) {
+                echo "Data inserted successfully";
+            } else {
+                die(mysqli_errno($con));
+            }
+
+        }
+
+        print_r($file_name_extension);
     }
 ?>
 
@@ -24,6 +43,37 @@
 <body>
     <div class="nwt-users">
         <h1>Users Data</h1>
+        <table>
+            <thead>
+                <tr>
+                    <th scope="col">SI Number</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Phone</th>
+                    <th scope="col">Image</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                    $sql = "Select * from `users`";
+                    $results = mysqli_query($con, $sql);
+                    while($row = mysqli_fetch_assoc($results)){
+                        $id = $row['id'];
+                        $name = $row['name'];
+                        $phone = $row['phone'];
+                        $image = $row['image'];
+
+                        echo '
+                            <tr>
+                                <th scope="row">'.$id.'</th>
+                                <td>'.$name.'</td>
+                                <td>'.$phone.'</td>
+                                <td><img src='.$image.' /></td>
+                            </tr>
+                        ';
+                    }
+                ?>
+            </tbody>
+        </table>
     </div>
 </body>
 </html>
